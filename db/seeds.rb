534e285a420f9acc6d1c46096d9f2dbeed787319
Benjamin Silva <benjamin@nexoit.com>
2016-06-23 22:37:22 -0300

    Agregado el seed para crear 25 profesores y 3 alumnos

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,21 @@
 
 
 25.times do |x|
-	
+	user = User.new
+	user.email = "profesor-#{x}@gmail.com"
+	user.password = 'password'
+	user.password_confirmation = 'password'
+	user.fullname = "Profesor #{x}"
+	user.user_type = 1
+	user.save!
+end
+
+3.times do |y|
+	user = User.new
+	user.email = "alumno-#{y}@gmail.com"
+	user.password = 'password'
+	user.password_confirmation = 'password'
+	user.fullname = "Alumno #{y}"
+	user.user_type = 2
+	user.save!
 end
