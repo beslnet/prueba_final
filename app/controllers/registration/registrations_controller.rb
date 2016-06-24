273e0d36a40c73019cad8e2b3f4307eddeb6c044
Benjamin Silva <benjamin@nexoit.com>
2016-06-23 21:59:07 -0300

    Se desarrolló la edición de usuarios, y se agregó el poder agregar y eliminar la foto del perfil de usuario

--- app/controllers/registration/registrations_controller.rb
+++ app/controllers/registration/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Registration::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
+  before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -44,9 +44,9 @@ class Registration::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:account_update) << [:fullname, :photo, :photo_delete]
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
